c2014d7079db9583c7c4c99def5bae5a9dc4e1f6
Nikolay Lanets <fi1osof@fi1osofski.modxclub.local>
2017-06-15 15:58:42 +0300

    fix

--- .structor/app/components.js
+++ .structor/app/components.js
@@ -2,17 +2,13 @@ import * as DataAware from 'modules/DataAware';
 import * as MUI from 'modules/MUI';
 import * as MUIIcons from 'modules/MUIIcons';
 import * as Utilities from 'modules/Utilities';
-import * as Test from 'modules/Test';
 import * as Templates from 'modules/Templates';
-
-console.log('Test', Test);
-console.log('Templates', Templates);
+  
 
 export {
     DataAware,
     MUI,
     MUIIcons,
     Utilities,
-    Test,
     Templates
 };
